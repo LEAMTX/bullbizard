6b32a2f661a2f8315652b7caab0275dc678837ed
lea <monthieuxlea@gmail.com>
2024-10-22 12:33:02 +0200

    bullbizard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,7 @@
+// Dans le fichier PokemonCard.tsx, crée un composant React appelé PokemonCard. Ce composant doit contenir une balise <figure>.
+function PokemonCard() {
+    return <figure> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" /></figure>;
+    <figcaption> "Bulbasaur" </figcaption>
+  }
+  
+  export default PokemonCard;
