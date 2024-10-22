37e5d8bfebb965188af8aff26d2b5096a194d8f6
lea <monthieuxlea@gmail.com>
2024-10-22 13:14:33 +0200

    envoit 2

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
+
+import MyTitle from "./MyTitle";
+
+
+
+function App() {
+  return (
+    <div><PokemonCard/> 
+    <MyTitle />
+    </div>
+  );
+}
+
+export default App;
+
+   
+
+
+
+
---
+++ src/MyTitle.tsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+    return <h1>bonjour bullbizard</h1>;
+  }
+  
+  export default MyTitle;
